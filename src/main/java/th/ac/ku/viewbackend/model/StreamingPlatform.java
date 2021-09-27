af2a32a25748c1044b0b2b2b874a4aae155c09b3
tnimeis7 <isreeya.n@ku.th>
2021-09-27 20:39:08 +0700

    Create streaming platform model

---
+++ src/main/java/th/ac/ku/viewbackend/model/StreamingPlatform.java
@@ -0,0 +1,23 @@
+package th.ac.ku.viewbackend.model;
+
+public class StreamingPlatform {
+
+    private String platformName;
+    private String picPath;
+
+    public String getPlatformName() {
+        return platformName;
+    }
+
+    public void setPlatformName(String platformName) {
+        this.platformName = platformName;
+    }
+
+    public String getPicPath() {
+        return picPath;
+    }
+
+    public void setPicPath(String picPath) {
+        this.picPath = picPath;
+    }
+}
