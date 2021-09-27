d5a38bf52feeba7b138fc79612c875d1ab4219b0
tnimeis7 <isreeya.n@ku.th>
2021-09-27 21:04:16 +0700

    create Streaming platform controller

---
+++ src/main/java/th/ac/ku/viewbackend/controller/StreamingPlatformController.java
@@ -0,0 +1,43 @@
+package th.ac.ku.viewbackend.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import th.ac.ku.viewbackend.model.Article;
+import th.ac.ku.viewbackend.model.StreamingPlatform;
+import th.ac.ku.viewbackend.service.ArticleService;
+
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+
+@RestController
+@RequestMapping("/stream")
+public class StreamingPlatformController {
+
+    @Autowired
+    private StreamingPlatformService streamingPlatformService;
+
+    @PostMapping
+    public String savePlatform(@RequestBody StreamingPlatform streamingPlatform) throws ExecutionException, InterruptedException {
+        return streamingPlatformService.savePlatform(streamingPlatform);
+    }
+
+    @GetMapping
+    public List<StreamingPlatform> getAllPlatform() throws ExecutionException, InterruptedException {
+        return streamingPlatformService.getAllPlatform();
+    }
+
+    @GetMapping("/{platformName}")
+    public Article getPlatform(@PathVariable String platformName) throws ExecutionException, InterruptedException {
+        return streamingPlatformService.getPlatform(platformName);
+    }
+
+    @PutMapping("/{platformName}")
+    public String updatePlatform(@RequestBody StreamingPlatform streamingPlatform) throws ExecutionException, InterruptedException {
+        return streamingPlatformService.updatePlatform(streamingPlatform);
+    }
+
+    @DeleteMapping("/{platformName}")
+    public String deletePlatform(@PathVariable String platformName) throws ExecutionException, InterruptedException {
+        return streamingPlatformService.deletePlatform(platformName);
+    }
+}
