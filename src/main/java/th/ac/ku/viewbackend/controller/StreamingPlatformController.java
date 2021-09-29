c8d6a9d1a29ed63e6a283a6363cc2dd1decd75c7
tnimeis7 <isreeya.n@ku.th>
2021-09-29 17:17:12 +0700

    Create Article Stream model

--- src/main/java/th/ac/ku/viewbackend/controller/StreamingPlatformController.java
+++ src/main/java/th/ac/ku/viewbackend/controller/StreamingPlatformController.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.concurrent.ExecutionException;
 
 @RestController
-@RequestMapping("/stream")
+@RequestMapping("/Stream")
 public class StreamingPlatformController {
 
     @Autowired
---
+++ src/main/java/th/ac/ku/viewbackend/model/ArticleStream.java
@@ -0,0 +1,22 @@
+package th.ac.ku.viewbackend.model;
+
+public class ArticleStream {
+    private String atcId;
+    private String platform;
+
+    public String getAtcId() {
+        return atcId;
+    }
+
+    public void setAtcId(String atcId) {
+        this.atcId = atcId;
+    }
+
+    public String getPlatform() {
+        return platform;
+    }
+
+    public void setPlatform(String platform) {
+        this.platform = platform;
+    }
+}
