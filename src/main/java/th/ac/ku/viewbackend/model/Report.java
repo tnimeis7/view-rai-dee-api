86379d07aba4c351fa9c1f1b2e4b802216096bb3
tnimeis7 <isreeya.n@ku.th>
2021-11-05 16:55:35 +0700

    change attribute in report model

--- src/main/java/th/ac/ku/viewbackend/model/Report.java
+++ src/main/java/th/ac/ku/viewbackend/model/Report.java
@@ -5,9 +5,9 @@ import com.google.cloud.Timestamp;
 public class Report implements BlockComponents{
     private String id;
     private String reportContent;
-    private String atcId;
+    private String mentionedId;
     private String reportBy;
-    private String commentId;
+    private String type; //article, comment
     private Timestamp reportDate;
 
     @Override
@@ -27,12 +27,12 @@ public class Report implements BlockComponents{
         this.reportContent = reportContent;
     }
 
-    public String getAtcId() {
-        return atcId;
+    public String getMentionedId() {
+        return mentionedId;
     }
 
-    public void setAtcId(String atcId) {
-        this.atcId = atcId;
+    public void setMentionedId(String mentionedId) {
+        this.mentionedId = mentionedId;
     }
 
     public String getReportBy() {
@@ -43,12 +43,12 @@ public class Report implements BlockComponents{
         this.reportBy = reportBy;
     }
 
-    public String getCommentId() {
-        return commentId;
+    public String getType() {
+        return type;
     }
 
-    public void setCommentId(String commentId) {
-        this.commentId = commentId;
+    public void setType(String type) {
+        this.type = type;
     }
 
     public Timestamp getReportDate() {
@@ -58,6 +58,4 @@ public class Report implements BlockComponents{
     public void setReportDate(Timestamp reportDate) {
         this.reportDate = reportDate;
     }
-
-
 }
