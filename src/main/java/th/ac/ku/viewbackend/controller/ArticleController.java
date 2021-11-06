50cfb48e2bd5399c532ce6564f543f36e4e4f99d
Pupae-ktsw <punthita.kh@ku.th>
2021-11-06 20:00:46 +0700

    most popular article query

--- src/main/java/th/ac/ku/viewbackend/controller/ArticleController.java
+++ src/main/java/th/ac/ku/viewbackend/controller/ArticleController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import th.ac.ku.viewbackend.model.Article;
 import th.ac.ku.viewbackend.model.BlockComponents;
+import th.ac.ku.viewbackend.service.ArticleService;
 import th.ac.ku.viewbackend.service.BlockService;
 
 import java.util.List;
@@ -16,6 +17,9 @@ public class ArticleController {
 
     @Autowired
     private BlockService service;
+
+    @Autowired
+    private ArticleService articleService;
 
     @PostMapping
     public BlockComponents saveArticle(@RequestBody Article article) throws ExecutionException, InterruptedException {
@@ -36,6 +40,11 @@ public class ArticleController {
     @DeleteMapping("/{atcId}")
     public String deleteArticle(@PathVariable String atcId) throws ExecutionException, InterruptedException {
         return service.delete(atcId, "Article");
+    }
+
+    @GetMapping("/MostPopular")
+    public List<BlockComponents> getAllArticlePopular() throws ExecutionException, InterruptedException {
+        return articleService.getPopularArticles();
     }
 
 }
---
+++ src/main/java/th/ac/ku/viewbackend/service/ArticleService.java
@@ -0,0 +1,32 @@
+package th.ac.ku.viewbackend.service;
+
+import com.google.api.core.ApiFuture;
+import com.google.cloud.firestore.*;
+import com.google.firebase.cloud.FirestoreClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import th.ac.ku.viewbackend.model.Article;
+import th.ac.ku.viewbackend.model.BlockComponents;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+
+@Service
+public class ArticleService {
+    @Autowired
+    private BlockService blockService;
+
+    public List<BlockComponents> getPopularArticles() throws ExecutionException, InterruptedException {
+        Firestore dbFirestore = FirestoreClient.getFirestore();
+        ApiFuture<QuerySnapshot> future = dbFirestore.collection("Article").orderBy("heart", Query.Direction.DESCENDING).get();
+        List<QueryDocumentSnapshot> documents = future.get().getDocuments();
+        List<BlockComponents> mostHeart = new ArrayList<>();
+        for(DocumentSnapshot document: documents) {
+            mostHeart.add(document.toObject(Article.class));
+        }
+        return mostHeart;
+    }
+
+
+}
